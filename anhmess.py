c47dbcc3cb5080cc7f61123ded265c6df2e358f7
phuonganhyeuem691-tech <phuonganhyeuem691@gmail.com>
2026-07-15 13:11:02 +0700

    Create anhmess.py

---
+++ anhmess.py
@@ -0,0 +1,7 @@
+# File anhmess.py dùng để giả lập và nạp các thành phần xử lý tin nhắn
+class NanhMessenger:
+    def __init__(self):
+        print("[anhmess] Khởi tạo mô-đun NanhMessenger thành công!")
+
+    def start_bot(self):
+        print("[anhmess] Bot Messenger đang được kích hoạt...")
